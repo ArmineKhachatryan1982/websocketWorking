e1968a0f000a0edceebb08a0b7217661b135f1a7
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-07-30 12:41:20 +0400

    add certificate

--- config/websockets.php
+++ config/websockets.php
@@ -116,17 +116,8 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
-
-      
-
-
-        /*
-         * Path to local private key file on filesystem in case of separate files for
-         * certificate (local_cert) and private key.
-         */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
-
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT'),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK'),
         /*
          * Passphrase for your local_cert file.
          */
